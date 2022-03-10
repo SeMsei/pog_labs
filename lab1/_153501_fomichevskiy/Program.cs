547a60d970b83d0d2ba76e447f4cdb0bf8c80a81
SeMsei <fomichevsjiy@gmail.com>
2022-03-10 03:34:22 +0300

    .

---
+++ lab1/_153501_fomichevskiy/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter 2 numbers:");
+int a, b, res;
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+res = a/b;
+Console.WriteLine("result = " + res);
+Console.WriteLine("Press any key...");
+Console.ReadKey();
